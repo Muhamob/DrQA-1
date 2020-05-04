b9831ce9105215f4b5e5b12da311064fd3b26bc9
muhamob <yusov.aa@phystech.edu>
2020-05-05 00:23:19 +0300

    ыыы

--- drqa/rnn_reader.py
+++ drqa/rnn_reader.py
@@ -102,7 +102,7 @@ class RnnDocReader(nn.Module):
             doc_hidden_size,
             question_hidden_size,
         )
-        self.projection = nn.Linear(2*opt['hidden_size'], 2)
+        self.projection = nn.Linear(doc_input_size + opt['hidden_size'], 2)
 
     def forward(self, x1, x1_f, x1_pos, x1_ner, x1_mask, x2, x2_mask):
         """Inputs:
